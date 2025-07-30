11a654ac33f3a9f4aef3a7b692fed3a6006119ef
Kyunsei <kirbyvaroujan@gmail.com>
2025-07-30 23:43:04 +0200

    added space

--- gmtk25_godot/ScenesScripts/player_spawner.gd
+++ gmtk25_godot/ScenesScripts/player_spawner.gd
@@ -5,9 +5,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("respawn") :
 		respawn()
 	
-	
-	
-
 
 func respawn():
 	var player_scene = load("res://ScenesScripts/player_cube.tscn")
