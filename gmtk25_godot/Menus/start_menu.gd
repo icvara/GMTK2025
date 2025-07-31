2e327e6197c368ceec0a8a0742a2649b7e7223e5
Kyunsei <kirbyvaroujan@gmail.com>
2025-07-31 18:41:13 +0200

    added level selector and level_02 for future prototype boss

--- gmtk25_godot/Menus/start_menu.gd
+++ gmtk25_godot/Menus/start_menu.gd
@@ -14,3 +14,11 @@ func _on_button_2_button_down() -> void:
 
 func _on_button_3_button_down() -> void:
 	get_tree().quit()
+
+
+func _on_button_4_button_down() -> void:
+	get_tree().change_scene_to_file("res://World/Level0/level_1.tscn")
+
+
+func _on_button_5_button_down() -> void:
+	get_tree().change_scene_to_file("res://World/Level0/level_2.tscn")
