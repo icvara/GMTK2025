e82ab37bc75931b99460f8541408808be99bbeaf
Kyunsei <kirbyvaroujan@gmail.com>
2025-08-03 14:20:43 +0200

    checkpoint is good

--- gmtk25_godot/check_point.gd
+++ gmtk25_godot/check_point.gd
@@ -2,7 +2,8 @@ extends StaticBody2D
 
 
 
-
+func _ready() -> void:
+	$AnimatedSprite2D.play()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and body.is_in_group("player"):
