a5d2e6ab77d836e8d3352892cf5b193826167805
Icvara <icvara.aor@gmail.com>
2025-08-03 14:50:26 +0100

    tp

--- gmtk25_godot/World/room/event_go_green_room.gd
+++ gmtk25_godot/World/room/event_go_green_room.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@export var TP :  Node2D
 
 
 func go_to_top_view(p):
@@ -9,7 +10,7 @@ func go_to_top_view(p):
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.TRANSITION_SCREEN_IN()
-		body.global_position = Vector2(300,3000)
+		body.global_position = TP.global_position
 		go_to_top_view(body)
 		body.power_is_available = true
 		body.power_ID =2 
